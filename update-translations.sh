df297ff780ccfce388c517db3c07b8a8169f2ee9
Cyrille Pontvieux <jrdATenialisDOTnet>
2011-06-08 18:56:57 +0000

    Support more FS. Translation updates.

--- update-translations.sh
+++ update-translations.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cd $(dirname $0)
 
-intltool-extract --type="gettext/ini" persistence-wizard.desktop.in
-intltool-extract --type="gettext/ini" persistence-wizard-kde.desktop.in
+intltool-extract --type="gettext/ini" src/persistence-wizard.desktop.in
+intltool-extract --type="gettext/ini" src/persistence-wizard-kde.desktop.in
 xgettext --from-code=utf-8 -L shell -o po/persistence-wizard.pot src/persistence-wizard
-xgettext --from-code=utf-8 -j -L C -kN_ -o po/persistence-wizard.pot persistence-wizard.desktop.in.h
-xgettext --from-code=utf-8 -j -L C -kN_ -o po/persistence-wizard.pot persistence-wizard-kde.desktop.in.h
+xgettext --from-code=utf-8 -j -L C -kN_ -o po/persistence-wizard.pot src/persistence-wizard.desktop.in.h
+xgettext --from-code=utf-8 -j -L C -kN_ -o po/persistence-wizard.pot src/persistence-wizard-kde.desktop.in.h
 
-rm persistence-wizard.desktop.in.h persistence-wizard-kde.desktop.in.h
+rm src/persistence-wizard.desktop.in.h src/persistence-wizard-kde.desktop.in.h
 
 cd po
 for i in `ls *.po`; do
