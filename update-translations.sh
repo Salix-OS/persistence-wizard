4f9d9d7bd7343e3f332c54b64a2068227762c643
Pierrick Le Brun <pierrickATsimplynuxDOTnet>
2011-06-09 11:21:31 +0000

    Corrected some formatting options, added complementary error checking, merged persistence-wizard-cli localization together with persistent-wizard licalization.

--- update-translations.sh
+++ update-translations.sh
@@ -4,6 +4,7 @@ cd $(dirname $0)
 intltool-extract --type="gettext/ini" src/persistence-wizard.desktop.in
 intltool-extract --type="gettext/ini" src/persistence-wizard-kde.desktop.in
 xgettext --from-code=utf-8 -L shell -o po/persistence-wizard.pot src/persistence-wizard
+xgettext --from-code=utf-8 -j -L shell -o po/persistence-wizard.pot src/persistence-wizard-cli 
 xgettext --from-code=utf-8 -j -L C -kN_ -o po/persistence-wizard.pot src/persistence-wizard.desktop.in.h
 xgettext --from-code=utf-8 -j -L C -kN_ -o po/persistence-wizard.pot src/persistence-wizard-kde.desktop.in.h
 
